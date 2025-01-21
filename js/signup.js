2b8ddfa4136d3b542b40f88eb8fb78ab2cd97210
pad1ryoshi <rodripadidev@gmail.com>
2025-01-21 20:00:43 -0300

    Adicionado a lógica de verificar se o e-mail já está registrado no banco de dados no arquivo signup.js.

--- js/signup.js
+++ js/signup.js
@@ -1,22 +1,38 @@
 document.querySelector('form').addEventListener('submit', async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const name = document.getElementById('signinNameInput').value;
-    const email = document.getElementById('signinEmailInput').value;
-    const password = document.getElementById('formSignUpPassword').value;
+  const name = document.getElementById('signinNameInput').value;
+  const email = document.getElementById('signinEmailInput').value;
+  const password = document.getElementById('formSignUpPassword').value;
 
-    const response = await fetch('http://localhost:3000/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
+  try {
+    
+      const getUsersResponse = await fetch('http://localhost:3000/users');
+      const users = await getUsersResponse.json();
 
-    if (response.ok) {
-      alert('Registro bem-sucedido!');
-      window.location.href = './login.html';
-    } else {
+      const userExists = users.some((user) => user.email === email);
+
+      if (userExists) {
+          alert('Email já registrado. Por favor, use outro email.');
+          return;
+      }
+
+      const response = await fetch('http://localhost:3000/users', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, email, password }),
+      });
+
+      if (response.ok) {
+          alert('Registro bem-sucedido!');
+          window.location.href = './login.html';
+      } else {
+          alert('Erro ao registrar. Tente novamente.');
+      }
+  } catch (error) {
+      console.error('Erro ao tentar registrar:', error);
       alert('Erro ao registrar. Tente novamente.');
-    }
-  });
+  }
+});
