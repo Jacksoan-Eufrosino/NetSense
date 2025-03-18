add07484fa6a6d3f31392444a2606e335326df40
pad1ryoshi <rodripadidev@gmail.com>
2025-03-18 17:55:43 -0300

    update: funcionalidade principal 80% implementada.

--- back/src/services/httpService.js
+++ back/src/services/httpService.js
@@ -1,25 +1,30 @@
-// src/services/httpService.js
 import axios from 'axios';
 import requestsRepository from '../models/requestsRepository.js';
 
 export default {
   async sendRequest(config, userId) {
     const startTime = Date.now();
-    
     try {
-      const response = await axios({
+      const axiosConfig = {
         method: config.method,
         url: config.url,
-        data: config.body,
+        timeout: 10000,
         headers: config.headers,
-        timeout: 10000
-      });
+      };
+
+      // Se não for GET/HEAD, adiciona body
+      if (!['GET', 'HEAD'].includes(config.method.toUpperCase()) && config.body) {
+        axiosConfig.data = config.body;
+      }
+
+      const response = await axios(axiosConfig);
 
       await requestsRepository.create({
         method: config.method,
         url: config.url,
         statusCode: response.status,
         response: response.data,
+        responseHeaders: response.headers,  // <--- Acho que essa parte do código vai conseguir trazer os headers da resposta !!! REMOVER O COMENTÁRIO!!!
         duration: Date.now() - startTime,
         userId
       });
@@ -28,7 +33,7 @@ export default {
         success: true,
         status: response.status,
         data: response.data,
-        headers: response.headers
+        headers: response.headers  // <--- Se quiser retornar direto
       };
     } catch (error) {
       console.error('Erro na requisição:', error);
